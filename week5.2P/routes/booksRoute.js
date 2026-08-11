498364a9151747173a78c5a489fa6c85333f6158
bristinaprajapati <bristinaprajapati@gmail.com>
2026-08-12 09:51:03 +1000

    5.2C folder name changes

--- week5.2P/routes/booksRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Controllers = require('../controllers');
-
-// Define routes for books
-router.get('/', Controllers.bookController.getAllBooks);
-router.get('/:id', Controllers.bookController.getBookById);
-
-module.exports = router;
